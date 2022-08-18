ae7f9adddc70db6365a71476f60eb7d1361ed864
William Roger <williamroger.frontend@gmail.com>
2022-08-18 11:59:20 -0300

    adicionado Styled Components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Welcome } from './src/components/Welcome';
+import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Welcome title="React Native Bare Workflow with TypeScript" />
-    </View>
-  );
+  return <Dashboard />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
