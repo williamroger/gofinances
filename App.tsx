c1ea38c27912169f49936cdb92c79f826b35ae3b
William Roger <williamroger.frontend@gmail.com>
2022-09-19 23:47:26 -0300

    configurado o Tab Navigation

--- App.tsx
+++ App.tsx
@@ -9,7 +9,9 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -38,7 +40,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
