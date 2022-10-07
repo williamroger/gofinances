ab251c87de0a0c8c42c01bfd153db7ca06fa67ea
William Roger <williamroger.frontend@gmail.com>
2022-10-07 13:52:34 -0300

    adicionada a tela resumo

---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { HistoryCard } from '../../components/HistoryCard';
+
+import {
+  Container,
+  Header,
+  Title,
+} from './styles';
+
+export function Resume() {
+  return (
+    <Container>
+      <Header>
+        <Title>Resumo por categoria</Title>
+      </Header>
+      <HistoryCard
+        title='Compras'
+        amount='R$ 100,00'
+        color='orange'
+      />
+    </Container>
+  );
+}
--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -22,20 +22,3 @@ export const Title = styled.Text`
   font-size: ${RFValue(18)}px;
   color: ${({ theme }) => theme.colors.shape};
 `;
-
-export const Form = styled.View`
-  flex: 1;
-  justify-content: space-between;
-  width: 100%;
-  padding: 24px;
-`;
-
-export const Fields = styled.View``;
-
-export const TransactionsType = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-
-  margin-top: 8px;
-  margin-bottom: 16px;
-`;
